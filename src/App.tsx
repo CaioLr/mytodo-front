87dac04ca56be9181be343c60cb1b1bb6d0862f9
CaioLr <56622666+CaioLr@users.noreply.github.com>
2023-06-16 15:26:18 -0300

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import { useState } from 'react'
+// import React from 'react';
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import Home from './pages/home/index';
+import Login from './pages/login/index';
+import Register from './pages/register/index';
 
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route index element={<Login />} />
+      <Route path="home" element={<Home />} />
+      <Route path="register" element={<Register />} />
+    </>
+  )
+)
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Opa!</h1>
+      <RouterProvider router={router}/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/register/index.tsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+const Register = () => {
+  return (
+    <div className="register-container">
+      Cadastro
+    </div>
+  );
+};
+
+export default Register;
